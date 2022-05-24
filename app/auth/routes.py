8a564cdaa398076e052d3e9351b4fc453d54c3f7
Craig Edwards <jamesallyncraigedwards@gmail.com>
2022-05-24 09:29:18 -0400

    Working_up_to_base--base_not_working

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__, template_folder='auth_templates', url_prefix='/auth')
 
+from .authforms import LoginForm
+
 @auth.route('/login')
 def login():
-    return 'testing'
+    lform = LoginForm
+    return render_template('signin.html', form=lform)
