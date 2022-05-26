b5ad60810ed97a5983f9bda1ebe246f16fad1f21
Craig Edwards <jamesallyncraigedwards@gmail.com>
2022-05-26 14:43:07 -0400

    get_all_animals_route_done

--- app/api/routes.py
+++ app/api/routes.py
@@ -6,5 +6,17 @@ from app.models import Animal
 
 @api.route("/test", methods=['GET'])
 def test():
+    fox = Animal.query.all()[0]
+    return jsonify(fox.to_dict()), 200
 
-    return jsonify('testing some data'), 200
+@api.route('/animals', methods = ['GET'])
+def getAnimals():
+    """
+    GET retrieves all animals from our database and returns them as JSON data
+    """
+    animals = Animal.query.all()
+    # animals = [a.to_dict() for a in animals]  list  version
+    animals = {a.species: a.to_dict() for  a in animals}  #dictionary version in comprehension form
+
+    return jsonify(animals), 200
+
